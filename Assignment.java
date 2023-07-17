ed68d7e06733978433ac268f17b573408e176d84
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-17 10:57:32 +0530

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,16 @@
+/* Assignment Operator Example...! */
+class Assignment
+{
+    public static void main(String[] args) {
+
+        int a;
+        a=10;  //Simple
+        System.out.println(a);
+
+        a+=10;  //Compound (a=a+10)
+        System.out.println(a);
+
+        a-=10;  //Compound (a=a-10)
+        System.out.println(a);
+    }
+}
